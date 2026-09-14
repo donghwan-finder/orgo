2c05f9d3623b6e5c277e78dc0dbf92b1945b2d12
donghwan-finder <dhcho668899@gmail.com>
2026-09-14 16:36:43 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,15 @@
-const C='orgo-v06-beside';const A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='orgo-v07';
+const ASSETS=['./','./index.html','./manifest.json','./orgo-icon-v2-192.png','./orgo-icon-v2-512.png'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
