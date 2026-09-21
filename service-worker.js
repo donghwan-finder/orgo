5048daa42230dd93f7bb92b5d9a87b15b0871c2f
donghwan-finder <dhcho668899@gmail.com>
2026-09-22 00:28:36 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='orgo-v08';
+const CACHE='orgo-v081';
 const ASSETS=['./','./index.html','./manifest.json','./orgo-icon-v2-192.png','./orgo-icon-v2-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
